59f1da516adaf3fdf5e767512cdf5bf2d1e29041
DiyaRamani <148998068+DiyaRamani@users.noreply.github.com>
2023-11-24 18:58:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use('/answer',answerRoutes)
 
 const PORT =process.env.PORT || 5000 
 
-const CONNECTION_URL = "mongodb+srv://Pradeep:aspd1234@cluster0.10dup2i.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
+const CONNECTION_URL = "mongodb+srv://diyaramani2004:<password>@cluster0.rvy2xy9.mongodb.net/"
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology :true})
 .then(()=>app.listen(PORT, () => {console.log(`server running on port on ${PORT}`)}))
